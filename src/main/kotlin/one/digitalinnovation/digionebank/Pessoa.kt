07cffe5fbf23c7ece6e7f1a2e26b54489c657a06
Vinicius Hilário <vinihdeo@gmail.com>
2022-07-26 20:05:57 -0300

    Testando uma inner class (Classe interna)

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,17 +1,28 @@
 package one.digitalinnovation.digionebank
 
+//Classe Pessoa com uma Classe interna
 class Pessoa {
 
     var nome:String = "Vinicius"
     var cpf:String = "123.123.123-12"
 
+    // Criando uma Classe Interna
+    inner class Endereco() {
+
+        var rua:String = "Rua Teste"
+
+    }
+
 }
 
-fun main(){
+//Função main para fazer um teste rápido
 
+fun main(){
     val vinicius = Pessoa()
 
     println("Nome: " + vinicius.nome)
     println("CPF: " + vinicius.cpf)
+
+    println("Rua: " + vinicius.Endereco().rua)
 
 }
