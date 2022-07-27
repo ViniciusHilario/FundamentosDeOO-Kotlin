67ac8257412e66fe8ec84caef7851206a588d115
Vinicius Hilário <vinihdeo@gmail.com>
2022-07-27 15:08:12 -0300

    Pegando as descrições no Enum e testanto

--- src/main/kotlin/one/digitalinnovation/digionebank/Testes/TesteClienteTipoEnum.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Testes/TesteClienteTipoEnum.kt
@@ -4,16 +4,24 @@ import one.digitalinnovation.digionebank.ClienteTipo
 
 fun main(){
 
+    //Pegando a descrição do Enum
     ClienteTipo.values().forEach { elemento ->
 
-        println("${elemento.name} - $elemento")
+        println("${elemento.name} - ${elemento.descricao}")
 
     }
 
     ClienteTipo.values().forEach {
 
-        println("${it.name} - $it")
+        println("${it.name} - ${it.descricao}")
 
     }
+
+    //Criando um teste para a descrção que foi pega
+    val pf = ClienteTipo.PF
+    println(">> ${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println(">>> ${pj.name} - ${pj.descricao}")
 
 }
